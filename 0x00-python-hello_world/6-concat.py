2fa8e17c0f91379ecfe89cedf88cd322246a0980
bayisa2022 <bayisa2022@gmail.com>
2022-11-30 14:05:59 +0000

    Task 5-7

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,4 @@
+word = "Holberton"
+print(f"First 3 letters: {word [:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
