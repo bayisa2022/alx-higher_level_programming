d0cdf25dd69f04e44818fc5413ebb1103deddaab
bayisa2022 <bayisa2022@gmail.com>
2022-11-30 20:45:45 +0000

    Tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,12 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
-    print()
+    new_text = ""
+    for i in (str):
+        char = ord(i)
+        if char >= ord('a') and char <= ord('z'):
+            char = char - 32
+            new_text = chr(char)
+        else:
+            new_text = i
+        print("{:s}".format(new_text), end="")
+    print("")
