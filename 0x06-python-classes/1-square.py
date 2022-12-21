1a21eb2a02cc78c1e4e0d5ea2c7c45080f27fea3
bayisa2022 <bayisa2022@gmail.com>
2022-12-21 15:07:33 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """define a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size 
