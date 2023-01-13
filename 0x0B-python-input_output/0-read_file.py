5aa694a95964e26d8e764d0b054c231a821092b0
bayisa2022 <bayisa2022@gmail.com>
2023-01-13 17:44:47 +0000

    Task3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3 
- """ 
- function that reads a text file 
- """ 
-  
-  
- def read_file(filename=""): 
-     """reads a text file (UTF8) and prints it to stdout 
-     Returns none 
-     """ 
-     with open(filename, "r", encoding="utf-8") as f: 
-         print(f.read(), end="")
+#!/usr/bin/python3
+
+'''Task 00 - 0. Read file'''
+
+
+def read_file(filename=""):
+    '''function that reads a text file (UTF8)
+    and prints it to stdout:'''
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end="")
