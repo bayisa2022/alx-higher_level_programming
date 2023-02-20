ccc6378b1427b1195a66ae9b3fa6ec3efbf67ee2
bayisa2022 <bayisa2022@gmail.com>
2023-02-20 08:06:29 +0000

    All  fine

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+
+
+-- removes all records where score is <= 5 from
+-- second table
+DELETE FROM second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- computes score average of all records in the second_table
+-- second table
+SELECT AVG(score) AS average FROM second_table;
+
