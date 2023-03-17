6e7989269eeb0ea3a09f915d462d626e652ce0d1
bayisa2022 <bayisa2022@gmail.com>
2023-03-17 08:57:39 +0000

    Tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,25 @@
-#!/usr/bin/python3
+!/usr/bin/python3
+""" filter states """
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
-
+def select_states():
+    """ access database print states start with N """
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
     cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
+
+if __name__ == "__main__":
+    select_states()
