b1f8b652aa0a2301d6b141969b29a50b9eb1e4cc
bayisa2022 <bayisa2022@gmail.com>
2023-03-18 17:39:36 +0000

    Task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,27 @@
-!/usr/bin/python3
-"""
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
-"""
-
+#!/usr/bin/python3
+'''takes in the name of a state as an argument and lists all
+ cities of that state, using the database hbtn_0e_4_usa'''
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    match_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
-
-        rows = cur.fetchall()
-
-    if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                AND states.name = '{}' \
+                ORDER BY cities.id ASC".format(match_name))
+    query_rows = cur.fetchall()
+    new_list = []
+    for row in query_rows:
+        new_list.append(row[0])
+    print(", ".join(new_list))
+    cur.close()
+    conn.close()
