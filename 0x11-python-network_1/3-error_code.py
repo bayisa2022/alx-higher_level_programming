3c4ad45febbe8016b4946f9d78dd12bafde405d9
bayisa2022 <bayisa2022@gmail.com>
2023-04-12 13:49:35 +0000

    Task3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-""" 
-Script that takes in a URL, send a request to URL, and dispaly body
 """
+Script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 
 
 if __name__ == "__main__":
-    import sys
-    from urllib import request, error
+    url = argv[1]
+    req = Request(url)
 
     try:
-        with request.urlopen(sys.argv[1]) as resp:
-            print(resp.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+        with urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
