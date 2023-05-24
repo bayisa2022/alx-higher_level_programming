b1edf1e48d9a3d168f6dadae423d4c344795f0d2
bayisa <bayisa2022@gmail.com>
2023-05-24 10:25:36 +0000

    Try 2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,12 @@
-const fs = require('fs');
+#!/usr/bin/node
+// A script that writes a string to a file
 
-function writeToFile(filePath, stringToWrite) {
-    try {
-        fs.writeFileSync(filePath, stringToWrite, 'utf-8');
-    } catch (error) {
-        console.error(error);
-    }
-}
+const fs = require('fs');
+const file = process.argv[2];
+const write_ = process.argv[3];
+
+fs.writeFile(file, write_, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
