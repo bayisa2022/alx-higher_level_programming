2974deb49ee0e13212e365d545f1d95370f43414
bayisa <bayisa2022@gmail.com>
2023-05-24 10:12:21 +0000

    Task -0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
-#!/usr/bin/node
-// A script that reads and prints the content of a file
-
-const file = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(file, 'utf8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
