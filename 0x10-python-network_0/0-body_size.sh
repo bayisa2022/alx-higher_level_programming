f4c3039676c757fd631e0a17bf56570b46c7e720
bayisa <bayisa2022@gmail.com>
2023-09-08 18:14:50 +0000

    Task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send request and display size of body
-curl -s -w "%{size_download}" -o /dev/null "$1"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
